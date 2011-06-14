df70121faf9358c0f4b0013d1695e051dec4991b
Tim Booher <ads@theboohers.org>
2011-06-13 20:41:40 -0400

    got bills working and tested

---
+++ test/fabricators/open_congress_bill.rb
@@ -0,0 +1,64 @@
+OpenStruct.new do |b|
+  # b.bill_titles {[{ "fti_titles" => "unknown",
+  #                   "title" => "Community Forestry Conservation Act of 2011",
+  #                   "is_default" => false,
+  #                   "title_type" => "short",
+  #                   "id" => 98118,
+  #                   "as" => "introduced",
+  #                   "bill_id" => 70740},{"fti_titles" => "\'bill\'2 \'bond\'12 \'conserv\'11 \'exempt\'8 \'feder\'6 \'forest\'10 \'provid\'4 \'purpos\'16 \'tax\'7",
+  #                   "title" => "A bill to provide a Federal tax exemption for forest conservation bonds, and for other purposes.",
+  #                   "is_default" => false,
+  #                   "title_type" => "official",
+  #                   "id" => 97659,
+  #                   "as" => "introduced",
+  #                   "bill_id" => 70740}]}
+  # b.bill_type "s"
+  # b.co_sponsors "need array here"
+  # b.id 70740
+  # b.introduced 1306386000
+  # b.last_speech 
+  # b.last_vote_date 
+  # b.last_vote_roll 
+  # b.last_vote_where 
+  # b.most_recent_actions {[{"result" => nil,
+  #                           "created_at" => "2011-05-27T134809Z",
+  #                           "govtrack_order" => "",
+  #                           "amendment_id" => "",
+  #                           "text" => "Read twice and referred to the Committee on Finance.",
+  #                           "date" =>  1306386000,
+  #                           "how" => "", 
+  #                           "id" => 298911,
+  #                           "vote_type" => "",
+  #                           "type" => "BillAction",
+  #                           "roll_call_id" => nil, 
+  #                           "action_type" => "action",
+  #                           "datetime" => "2011-05-26T000000Z",
+  #                           "where" => nil, 
+  #                           "bill_id" => 70740,
+  #                           "roll_call_number" => nil} ,
+  #                         {"result" => nil,
+  #                           "created_at" => "2011-05-27T134809Z",
+  #                           "govtrack_order" => nil,
+  #                           "amendment_id" => nil,
+  #                           :text => nil,
+  #                           :date => 1306386000,
+  #                           :how => nil,
+  #                           :id => 298910,
+  #                           :vote_type => nil, 
+  #                           :type => "BillAction",
+  #                           :roll_call_id => nil,
+  #                           :action_type => "introduced",
+  #                           :datetime => "2011-05-26T000000Z",
+  #                           :where => nil,
+  #                           :bill_id => 70740,
+  #                           :roll_call_number => nil}
+  #                        ]}
+  # b.number 1105
+  # b.plain_language_summary 
+  # b.recent_blogs []
+  # b.recent_news []
+  # b.session 112
+  #b.sponsor !ruby/objectGovKitOpenCongressPerson {}
+  b.status = "Introduced"
+  b.title_full_common = "S.1105 Community Forestry Conservation Act of 2011"
+end
