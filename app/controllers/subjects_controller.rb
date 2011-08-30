fd8e1d8f9cad9e10e8a541cf61453c329bc31125
Nathan Janssen <c11nathan.janssen@gmail.com>
2011-08-29 20:36:05 -0600

    legislator/show

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -16,7 +16,7 @@ class SubjectsController < ApplicationController
     @subject = Subject.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html # show.haml
       format.json { render json: @subject }
     end
   end
