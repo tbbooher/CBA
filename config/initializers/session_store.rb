e9390ac920fbc94b1d4531fa9e4066053b3666e8
Tim Booher <ads@theboohers.org>
2011-09-03 08:50:10 -0400

    back to cookie store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,12 +1,8 @@
 # -*- encoding : utf-8 -*-
 
 # Be sure to restart your server when you modify this file.
-require "mongo_session_store/mongoid"
 
-Cba::Application.config.session_store :mongoid_store, :key => '_cba_session'
-#Cba::Application.config.session_store :cookie_store, :key => '_cba_session'
-
-# TODO --
+Cba::Application.config.session_store :cookie_store, :key => '_cba_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
