ff8e114602f2d229d6048d2e1aff42148b17aa07
c11nathanjanssen <c11nathan.janssen@gmail.com>
2011-09-14 20:11:22 -0600

    ajax

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -4,7 +4,8 @@ class LegislatorsController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   def index
-    @legislators = Legislator.all.search(params[:search]).sort(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page =>  20)
+    #.order(sort_column + " " + sort_direction)
+    @legislators = Legislator.all.search(params[:search]).paginate(:page => params[:page], :per_page =>  20)
     @bills = Bill.all.paginate(:page => params[:page], :per_page => 20)
 
     respond_to do |format|
