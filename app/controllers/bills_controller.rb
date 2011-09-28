69874952c2029eb181f5bf53f9feb9acbef644f1
Tim Booher <ads@theboohers.org>
2011-09-28 13:52:24 -0400

    test

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -4,8 +4,8 @@ class BillsController < ApplicationController
   # GET /bills
   # GET /bills.xml
   def index
-    #@bills = Bill.page(1).per(10)
-    @bills = Bill.all.paginate(:page => params[:page], :per_page => 20)
+    #@bills = Bill.all.paginate(:page => params[:page], :per_page => 20)
+    @bills = Bill.all.select{|b| b.activity?}
 
     respond_to do |format|
       format.html # index.haml
