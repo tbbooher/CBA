92e4e7bd7b9c7bd6c609f0df221b7a905748394c
Tim Booher <ads@theboohers.org>
2011-09-29 06:14:01 -0400

    updated lists

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,12 @@
 # -*- encoding : utf-8 -*-
 
 module UsersHelper # :nodoc:
+
+  def links_to_groups(user_groups)
+    groups = []
+    user_groups.each do |g|
+      groups << link_to(g.name, g)
+    end
+    groups.to_sentence
+  end
 end
