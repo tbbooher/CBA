cfabf27ef6da565ed3ddcefb4956e61710a7f8c4
c11nathanjanssen <c11nathan.janssen@gmail.com>
2011-10-04 03:04:14 +0000

    update

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -29,10 +29,10 @@ class BillsController < ApplicationController
   # GET /bills/1.xml
   def show
   #modified by nate
-    @districts = PolcoGroup.district_search(params[:district_search]).districts.where(:vote_count.gt => 0).desc(:member_count).paginate(:page => params[:page], :per_page => 10)
+    @districts = PolcoGroup.districts.where(:vote_count.gt => 0).desc(:member_count).paginate(:page => params[:page], :per_page => 10)
     @user = current_user
     # need to remove extra data
-    @PolcoGroups=@user.polcogroup_search(params[:polcogroup_search]).non_district_groups_for_bill(@bill).paginate(:page => params[:page], :per_page => 10)
+    @PolcoGroups=@user.non_district_groups_for_bill(@bill).paginate(:page => params[:page], :per_page => 10)
     #@PolcoGroups=@user.all_groups_for_bill(@bill).paginate(:page => params[:page], :per_page => 10)
     @rolled = (@bill.member_votes.size > 0)
 
