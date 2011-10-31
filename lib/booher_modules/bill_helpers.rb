ddfdb878035154a662c911859c18dd5e804fcf5b
Tim Booher <ads@theboohers.org>
2011-10-31 13:45:11 -0400

    fixed bundler cap file with whenever command

--- lib/booher_modules/bill_helpers.rb
+++ lib/booher_modules/bill_helpers.rb
@@ -8,9 +8,19 @@ module BillHelpers
       # if it is destroy all the previous votes and add these votes in
       # why in the heck would this happen
       #
-
+    files.each do |bill_path|
+      f = File.new(bill_path, 'r')
+      if we_need_to_look_at_it(f)
+        # get feed
+        feed = Feedzirra::Parser::RollCall.parse(f)
+      end
+    end
 
   end
 
+  def we_need_to_look_at_it(file_handle)
+    # does the bill exist? && have we already read this file?
+    # return true
+  end
 
 end
