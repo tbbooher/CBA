552537396194d85a0487dc85e2d0498a76561720
Tim Booher <ads@theboohers.org>
2012-04-07 09:12:39 -0400

    updated

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,25 +1,25 @@
 # -*- encoding : utf-8 -*-
 
-require 'openid/store/filesystem'
-require File.join(Rails.root,'config/omniauth_settings')
+#require 'openid/store/filesystem'
+#require File.join(Rails.root,'config/omniauth_settings')
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  tmp_dir = File.expand_path('../../../tmp/openid.store', __FILE__)
+  #tmp_dir = File.expand_path('../../../tmp/openid.store', __FILE__)
 
-  if defined? OMNIAUTH_GOOGLE
-    provider :google_apps, OpenID::Store::Filesystem.new(tmp_dir), :domain => 'gmail.com'
+  if defined? ENV['OMNIAUTH_GOOGLE']
+    #provider :google_apps, OpenID::Store::Filesystem.new(tmp_dir), :domain => 'gmail.com'
   end
   
-  if defined? OMNIAUTH_OPENID
-    provider :open_id, OpenID::Store::Filesystem.new(tmp_dir)
+  if defined? ENV['OMNIAUTH_OPENID']
+    #provider :open_id, OpenID::Store::Filesystem.new(tmp_dir)
   end
-  provider( :twitter, OMNIAUTH_TWITTER_KEY, OMNIAUTH_TWITTER_SECRET, {:client_options => {:ssl => {:ca_path => OMNIAUTH_CAPATH}}} ) if defined?(OMNIAUTH_TWITTER_KEY)
-  provider( :facebook, OMNIAUTH_FACEBOOK_KEY, OMNIAUTH_FACEBOOK_SECRET) if defined?(OMNIAUTH_FACEBOOK_KEY)
-  provider( :linked_in, OMNIAUTH_LINKED_IN_KEY, OMNIAUTH_LINKED_IN_SECRET) if defined?(OMNIAUTH_LINKED_IN_KEY)
-  #provider( :thirty_seven_signals, OMNIAUTH_BASECAMP_ID, OMNIAUTH_BASECAMP_SECRET) if defined?(OMNIAUTH_BASECAMP_ID)
-  provider( :github, OMNIAUTH_GITHUB_ID, OMNIAUTH_GITHUB_SECRET) if defined?(OMNIAUTH_GITHUB_ID)
-  provider( :campus, OMNIAUTH_CAMPUS_ID, OMNIAUTH_CAMPUS_SECRET, OMNIAUTH_CAMPUS_URL) if defined?(OMNIAUTH_CAMPUS_ID)
-  #provider :LDAP, "LDAP-Login #{LDAP_HOST}", { :host => LDAP_HOST, :port => LDAP_PORT, :method => :plain,
+  provider( :twitter, ENV['OMNIAUTH_TWITTER_KEY'], ENV['OMNIAUTH_TWITTER_SECRET'], {:client_options => {:ssl => {:ca_path => ENV['OMNIAUTH_CAPATH']}}} ) if defined?(ENV['OMNIAUTH_TWITTER_KEY'])
+  provider( :facebook, ENV['OMNIAUTH_FACEBOOK_KEY'], ENV['OMNIAUTH_FACEBOOK_SECRET']) if defined?(ENV['OMNIAUTH_FACEBOOK_KEY'])
+  provider( :linked_in, ENV['OMNIAUTH_LINKED_IN_KEY'], ENV['OMNIAUTH_LINKED_IN_SECRET']) if defined?(ENV['OMNIAUTH_LINKED_IN_KEY'])
+  #provider( :thirty_seven_signals, ENV['OMNIAUTH_BASECAMP_ID'], ENV['OMNIAUTH_BASECAMP_SECRET']) if defined?(ENV['OMNIAUTH_BASECAMP_ID'])
+  provider( :github, ENV['OMNIAUTH_GITHUB_ID'], ENV['OMNIAUTH_GITHUB_SECRET']) if defined?(ENV['OMNIAUTH_GITHUB_ID'])
+  provider( :campus, ENV['OMNIAUTH_CAMPUS_ID'], ENV['OMNIAUTH_CAMPUS_SECRET'], ENV['OMNIAUTH_CAMPUS_URL']) if defined?(ENV['OMNIAUTH_CAMPUS_ID'])
+  #provider :ENV['LDAP'], "LDAP-Login #{LDAP_HOST}", { :host => LDAP_HOST, :port => LDAP_PORT, :method => :plain,
   #         :base => LDAP_TREEBASE, :uid => 'uid', :bind_dn => "uid=%s,cn=users,dc=xs1,dc=intern,dc=wwedu,dc=com" }
 end
 
